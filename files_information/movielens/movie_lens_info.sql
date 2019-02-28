f10dcecdc20c24beb07384f478af30a0f92939fa
licunzhi <2718272293@qq.com>
2019-02-28 21:55:09 +0800

    Initial commit

---
+++ files_information/movielens/movie_lens_info.sql
@@ -0,0 +1,3 @@
+INSERT INTO movie_lens.info (counts, name) VALUES (943, 'users');
+INSERT INTO movie_lens.info (counts, name) VALUES (1682, 'items');
+INSERT INTO movie_lens.info (counts, name) VALUES (100000, 'ratings');
